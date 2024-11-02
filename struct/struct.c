1e376aea086a3d71ec416126b3b91d1041b9cfd9
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2024-11-02 19:49:16 +0530

    Updated code

--- struct/struct.c
+++ struct/struct.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+
+// Declared struct 
+// for student
 struct student{
     int roll;
     float cgpa;
     char name[100];
 };
+
 int main(){
-struct student s1;
-s1.roll = 166;
-s1.cgpa = 9.4;
-strcpy(s1.name,"dhruv");//to store the name in the string
-printf("%d %f %s \n",s1.roll,s1.cgpa,s1.name);
-struct student s2;
-strcpy(s2.name,"ok");
-printf("%s",s2.name);
+    struct student s1;
+    s1.roll = 166;
+    s1.cgpa = 9.4;
+
+    strcpy(s1.name,"dhruv"); //to store the name in the string
+    printf("%d %f %s \n",s1.roll,s1.cgpa,s1.name);
+    
+    struct student s2;
+    strcpy(s2.name,"ok");
+    printf("%s",s2.name);
+
 }
