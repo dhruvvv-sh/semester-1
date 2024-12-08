b13de969f899b7b7e00f5da3732ecb72382298b0
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2024-12-08 12:15:31 +0530

    first code for python

---
+++ python/intialcode.py
@@ -0,0 +1,14 @@
+name = "dhruv"
+print("my name is :",name)
+num1 = 2
+num2 = 5
+sum = num1 + num2
+print(sum)
+num3 = "3" #doesnt take ascii values 
+b = int(num3)
+print(num1+b)
+a = input("enter your name:")
+print("your name is :",a)
+marks = int(input("enter your marks:"))
+age = int(input("enter your age:"))
+print(f"your marks is:{marks} and your age is:{age}") #using f strings 
